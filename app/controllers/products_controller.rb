dc3d9449217e00b4e97e5071c9141a96c5ddeb82
nsudhanva <nsudhanva@gmail.com>
2017-03-20 14:46:03 +0530

    Added icons

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :code, :description, :image, :cost, :quantity)
+      params.require(:product).permit(:name, :code, :description, :image, :cost, :quantity, category_ids: [])
     end
 end
